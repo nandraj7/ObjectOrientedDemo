1840ffb20299616856a5a3aa89015d632f4c306a
hp <hp@HPELITEBOOK840G>
2023-03-03 18:47:15 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedDemo
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+
+        public void GetData()
+        {
+            Console.WriteLine("Please Enter the Value for employee Id");
+            this.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Value for employee Name");
+            this.Name = Console.ReadLine();
+
+            Console.WriteLine("Please Enter the Value for employee Address");
+            this.Address = Console.ReadLine();
+        }
+
+        public void PutData()
+        {
+
+            Console.WriteLine("Id=" + this.Id);
+            Console.WriteLine("Name=" + this.Name); 
+            Console.WriteLine("Address=" + this.Address);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using ObjectOrientedDemo;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Employee1 Object Information");
+        Employee employee1 = new Employee();
+        employee1.GetData();
+        employee1.PutData();
+
+
+        CreateDemoObject();
+    }
+
+    public static void CreateDemoObject()
+    {
+        Employee object1 = new Employee();
+
+        object1.Id = 10;
+        object1.Name = "Amit";
+        object1.Address = "Pune";
+
+        Console.WriteLine("Object1 Information Id=" + object1.Id);
+        Console.WriteLine("Name=" + object1.Name);
+        Console.WriteLine("Address=" + object1.Address);
+
+
+        Employee object2;
+        object2 = new Employee();
+
+        object2.Id = 20;
+        object2.Name = "Vijay";
+        object2.Address = "Mumbai";
+
+        Console.WriteLine("object2 Information Id=" + object2.Id);
+        Console.WriteLine("Name=" + object2.Name);
+        Console.WriteLine("Address=" + object2.Address);
+
+
+        Employee employee = new Employee();
+
+        Console.WriteLine("Please Enter the Value for employee Id");
+        employee.Id = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please Enter the Value for employee Name");
+        employee.Name = Console.ReadLine();
+
+        Console.WriteLine("Please Enter the Value for employee Address");
+        employee.Address = Console.ReadLine();
+
+
+        Console.WriteLine("employee Information Id=" + employee.Id);
+        Console.WriteLine("Name=" + employee.Name);
+        Console.WriteLine("Address=" + employee.Address);
+    }
+
+
+}
+
